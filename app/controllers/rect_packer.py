d540bca040211f15b539b06339e91d76ef226043
SvyatoslavArtymovych <svyatoslav@gmail.com>
2022-11-15 12:39:12 +0200

    add pack and generate_image_for_bin to RectPacker

--- app/controllers/rect_packer.py
+++ app/controllers/rect_packer.py
@@ -1,17 +1,23 @@
 from rectpack import newPacker, guillotine
+from PIL import Image, ImageDraw
 
 
 class RectPacker:
-    def __init__(self):
+    def __init__(self, blade_size: int = 0):
         self.packer = newPacker(pack_algo=guillotine.GuillotineBssfMaxas)
         self.bins = []
         self.rectangles = []
+        self.blade_size = blade_size
+        self.result = {
+            "not_placed_rectangles": [],
+            "bins": [],
+        }
 
-    def add_bin(self, width: float, height: float):
+    def add_bin(self, width: int, height: int):
         self.bins.append([width, height])
 
-    def add_rectangle(self, width: float, height: float):
-        self.rectangles.append([width, height])
+    def add_rectangle(self, width: int, height: int):
+        self.rectangles.append(sorted([width, height]))
 
     def validate_rectangles(self):
         if not self.bins:
@@ -25,9 +31,84 @@ class RectPacker:
             fit_in_bins = []
             for bin in self.bins:
                 bin = sorted(bin)
-                fit_in_bins.append(rect[0] < bin[0] and rect[1] < bin[0])
+                fit_in_bins.append(
+                    rect[0] + self.blade_size * 2 < bin[0]
+                    and rect[1] + self.blade_size * 2 < bin[0]
+                )
             if not any(fit_in_bins):
                 invalid_rectangles.append(rect)
 
         if invalid_rectangles:
             raise ValueError(f"Found invalid rectangle(s): {invalid_rectangles}")
+
+    def pack(self):
+        for bin in self.bins:
+            self.packer.add_bin(*bin)
+        for rect in self.rectangles:
+            rect = [
+                rect[0] + self.blade_size * 2,
+                rect[1] + self.blade_size * 2,
+            ]
+            self.packer.add_rect(*rect)
+
+        self.packer.pack()
+
+        not_placed_rectangles = [sorted(rect) for rect in self.rectangles]
+        for bin in self.packer:
+            bin_result = {
+                "sizes": [bin.width, bin.height],
+                "rectangles": [],
+                "used_area": 0,
+                "wasted_area": 0,
+                "image": None,
+            }
+            for rect in bin:
+                rect = sorted(
+                    [
+                        rect.width - self.blade_size * 2,
+                        rect.height - self.blade_size * 2,
+                    ]
+                )
+                bin_result["rectangles"].append(rect)
+                not_placed_rectangles.remove(rect)
+                bin_result["used_area"] += rect[0] * rect[1]
+            bin_result["wasted_area"] = bin.width * bin.height - bin_result["used_area"]
+
+            self.result["bins"].append(bin_result)
+
+            bin_result["image"] = self.generate_image_for_bin(bin)
+
+        self.result["not_placed_rectangles"] = not_placed_rectangles
+
+    def generate_image_for_bin(self, bin):
+        shape = [(bin.width, bin.height), 0, 0]
+
+        img = Image.new("RGB", (bin.width, bin.height), "white")
+
+        img_draw = ImageDraw.Draw(img)
+        img_draw.rectangle(shape, outline="black")
+
+        for rect in bin:
+            rectangle = [
+                (rect.x, rect.y),
+                (rect.x + rect.width, rect.y + rect.height),
+            ]
+            img_draw.rectangle(
+                rectangle,
+                outline="white" if self.blade_size else "black",
+                fill="white" if self.blade_size else "#efe6e6",
+            )
+            if self.blade_size:
+                rectangle = [
+                    (rect.x + self.blade_size, rect.y + self.blade_size),
+                    (
+                        rect.x + rect.width - self.blade_size,
+                        rect.y + rect.height - self.blade_size,
+                    ),
+                ]
+                img_draw.rectangle(
+                    rectangle,
+                    outline="black",
+                    fill="#efe6e6",
+                )
+        return img
--- tests/test_rect_packer.py
+++ tests/test_rect_packer.py
@@ -40,3 +40,71 @@ def test_basic_rect_packer_functionality(rect_packer: RectPacker):
     # validate with larger bin
     rect_packer.add_bin(1500, 1500)
     rect_packer.validate_rectangles()
+
+    # validate with blade size
+    rect_packer.blade_size = 400
+    with pytest.raises(ValueError):
+        rect_packer.validate_rectangles()
+
+    rect_packer.blade_size = 100
+    rect_packer.validate_rectangles()
+
+    rect_packer.pack()
+
+    result = rect_packer.result
+
+    for bin in result["bins"]:
+        assert "image" in bin
+        assert bin["image"]
+        assert "sizes" in bin
+        assert bin["sizes"] in rect_packer.bins
+        assert "rectangles" in bin
+        for rect in bin["rectangles"]:
+            assert sorted(rect) in rect_packer.rectangles
+
+        bin_area = bin["sizes"][0] * bin["sizes"][1]
+        assert bin_area
+        assert "used_area" in bin
+        assert bin["used_area"] <= bin_area
+        assert "wasted_area" in bin
+        assert bin["wasted_area"] <= bin_area
+
+    assert "not_placed_rectangles" in result
+    assert len(result["not_placed_rectangles"]) <= len(rect_packer.rectangles)
+
+
+def test_with_summary_rects_area_larger_than_bin_area(rect_packer: RectPacker):
+    BIN = [1000, 1000]
+    BIN_AREA = BIN[0] * BIN[1]
+    RECTS_AREA = 0
+    rect_packer.add_bin(*BIN)
+
+    while RECTS_AREA <= BIN_AREA:
+        RECT = [150, 210]
+        RECTS_AREA += RECT[0] * RECT[1]
+        rect_packer.add_rectangle(*RECT)
+
+    rect_packer.validate_rectangles()
+
+    rect_packer.pack()
+
+    result = rect_packer.result
+
+    for bin in result["bins"]:
+        assert "image" in bin
+        assert bin["image"]
+        assert "sizes" in bin
+        assert bin["sizes"] in rect_packer.bins
+        assert "rectangles" in bin
+        for rect in bin["rectangles"]:
+            assert sorted(rect) in rect_packer.rectangles
+
+        bin_area = bin["sizes"][0] * bin["sizes"][1]
+        assert bin_area
+        assert "used_area" in bin
+        assert bin["used_area"] <= bin_area
+        assert "wasted_area" in bin
+        assert bin["wasted_area"] <= bin_area
+
+    assert "not_placed_rectangles" in result
+    assert len(result["not_placed_rectangles"]) <= len(rect_packer.rectangles)
