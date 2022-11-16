dbeadf8d6fe6be8e18a4a1bc14f1967d7e41010e
SvyatoslavArtymovych <svyatoslav@gmail.com>
2022-11-16 20:03:27 +0200

    small left bar changes

--- app/static/js/calculator/actions.js
+++ app/static/js/calculator/actions.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const addedRectsDiv = document.querySelector(".added-rects");
   const bladeSizeInput = document.querySelector(".blade-size");
   const printPriceInput = document.querySelector(".print-price");
+  const meticSystemSelect = document.querySelector(".metic-system");
 
   cleanAllBtn.addEventListener("click", () => {
     addedBinsDiv.innerHTML = "";
     addedRectsDiv.innerHTML = "";
     bladeSizeInput.value = 0;
     printPriceInput.value = 0;
+    meticSystemSelect.disabled = false;
   });
 });
