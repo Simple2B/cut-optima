d9f8ab5b1cd79d148df3210dd4e6d2fc2cde7ff7
SvyatoslavArtymovych <svyatoslav@gmail.com>
2022-11-21 11:00:41 +0200

    uncomment login_required on calculator page

--- app/views/calculator.py
+++ app/views/calculator.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import login_user
 
-# from flask_login import login_user
 from config import BaseConfig as conf
 
 blueprint = Blueprint("calculator", __name__)
 
 
-# @login_required
+@login_required
 @blueprint.route("/calculator", methods=["GET"])
-def register():
+def calculator():
     return render_template("calculator/index.html", config=conf)
